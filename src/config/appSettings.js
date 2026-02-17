80e5c1137d285b289658ca141e97ae443416f674
FahadTechmenders <241767225+FahadTechmenders@users.noreply.github.com>
2026-02-17 17:32:47 +0500

    set appsettings

--- src/config/appSettings.js
+++ src/config/appSettings.js
@@ -1,11 +1,11 @@
 const appSettings = {
   // Environment: 'development' | 'production'
-  environment: 'production',
+  environment: 'development',
   
   // API Configuration
   api: {
-    baseUrl: 'https://localhost:7043/api',
-    // baseUrlLocal: 'https://localhost:7043/api',
+    baseUrl: 'https://classpediaadminapi.thetechmenders.com/api',
+    baseUrlLocal: 'https://localhost:7043/api',
     chatbotUrl: 'https://chatbot.thetechmenders.com/api',
     timeout: 30000,
     retryAttempts: 3,
