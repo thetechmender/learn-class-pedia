72343bfdbe97eeb49c59cc9d7e7d0198657aa8fc
usama-khalied <usamakhalied@gmail.com>
2026-04-14 16:03:31 +0500

    Added Dummy Assessment Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,6 +23,10 @@ export const routes: Routes = [
     loadComponent: () => import('./setup/course/course').then(m => m.CourseComponent)
   },
   {
+    path: 'dummy',
+    loadComponent: () => import('./setup/assessment/dummy-cleared-assessment/dummy-cleared-assessment').then(m => m.DummyClearedAssessment)
+  },
+  {
     path: '**',
     redirectTo: 'course'
   }
---
+++ src/app/setup/assessment/dummy-cleared-assessment/dummy-cleared-assessment.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-cleared-assessment',
+  imports: [],
+  templateUrl: './dummy-cleared-assessment.html',
+  styleUrl: './dummy-cleared-assessment.sass',
+})
+export class DummyClearedAssessment {
+
+}
