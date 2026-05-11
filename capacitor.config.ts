f869648ed51710f0ae76d19010e73fd49eb5bb3d
FahadTechmenders <241767225+FahadTechmenders@users.noreply.github.com>
2026-05-11 19:01:04 +0500

    add android files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learnclasspedia.app',
+  appName: 'Learn Class Pedia',
+  webDir: 'dist/learn-class-pedia/browser',
+};
+
+export default config;
